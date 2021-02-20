bd933ad08c62aae734f8ec8e82af7c361b5995b5
lijieniu <niulijie19890522@163.com>
2021-02-20 19:59:46 +0800

    init project

---
+++ src/pages/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>{{counter}}</div>
+</template>
+
+<script lang="ts">
+import {computed, ref} from 'vue';
+export default {
+    name: 'App',
+    setup() {
+        let counter = ref(1);
+        // setInterval(() => {
+        //     counter.value++;
+        //     console.log(counter);
+        // }, 1000);
+        return {counter}
+    }
+}
+</script>
+
