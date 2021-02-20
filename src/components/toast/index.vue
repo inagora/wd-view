d1a70b149a76c4d30189472ab8b9f467b1b5cca8
lijieniu <niulijie19890522@163.com>
2021-02-20 20:42:29 +0800

    toast 组件

---
+++ src/components/toast/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-show="showToast">{{msg}}</div>
+</template>
+
+<script lang="ts">
+enum POSITION {
+    top,
+    center,
+    bottom
+}
+import {ref, watch} from 'vue';
+export default {
+    name: 'toast',
+    props: {
+        msg: String,
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        duration: {
+            type: Number,
+            default: 3000
+        }
+    },
+    setup(props) {
+        console.log(props);
+        let showToast = ref(false);
+        watch(props.visible, () => {
+            showToast = ref(props.visible);
+        });
+        setTimeout(() => {
+            showToast.value = false;
+        }, props.duration);
+        return {
+            showToast
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,18 +1,40 @@
 <template>
-    <div>{{counter}}</div>
+    <div>
+        <toast 
+            :visible="isShowToast"
+            :msg="toastMsg"
+            :duration="duration"/>
+        <button @click="doShowToastClickHandler">show toast</button>
+    </div>
 </template>
 
 <script lang="ts">
 import {computed, ref} from 'vue';
+import Toast from '../components/toast/index.vue';
 export default {
     name: 'App',
+    components: {
+        Toast
+    },
     setup() {
         let counter = ref(1);
+        let isShowToast = ref(false);
+        let toastMsg = ref('');
+        let duration = ref(0);
+        // setTimeout(() => {
+        //     isShowToast.value = true;
+        //     toastMsg.value = '这里是toast内容';
+        // }, 3000);
+        const doShowToastClickHandler = () => {
+            isShowToast.value = true;
+            toastMsg.value = '这里是toast内容';
+            duration.value = 1000;
+        };
         // setInterval(() => {
         //     counter.value++;
         //     console.log(counter);
         // }, 1000);
-        return {counter}
+        return {counter, isShowToast, toastMsg, doShowToastClickHandler, duration};
     }
 }
 </script>
