df2bedfd266699f893eb66cfde0f89d4ae0f3bd2
lijieniu <niulijie19890522@163.com>
2021-07-19 21:29:51 +0800

    popper

--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -1,8 +1,10 @@
 import {App} from 'vue';
 import Select from './select.vue';
+import WdPopper from '../popper/src/index.vue';
 
 const install = (app: App) => {
     app.component(Select.name, Select);
+    app.component(WdPopper.name, WdPopper);
 };
 
 export default {
