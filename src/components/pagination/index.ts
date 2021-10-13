904463bc4c79291e2156db0d2731f8dd64233862
lijieniu <niulijie19890522@163.com>
2021-10-13 21:19:33 +0800

    table pagesize

--- src/components/pagination/index.ts
+++ src/components/pagination/index.ts
@@ -1,7 +1,9 @@
 import {App} from 'vue';
 import WdPagination from './pagination.vue';
+import WdSelect from '../select/index';
 const install = (app: App) => {
     app.component(WdPagination.name, WdPagination);
+    app.component((WdSelect as any).name, WdSelect);
 };
 
 export default {
