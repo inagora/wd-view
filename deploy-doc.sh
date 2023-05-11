f8a5fb4ec0d65fa91650e67f32b0620da11b8f49
lijieniu <niulijie19890522@163.com>
2023-05-11 16:31:36 +0800

    deploy docs

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy docs'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:inagora/wd-view.git master:gh-pages
+git push -f git@github.com:inagora/wd-view.git gh-pages
 
 cd -
