5cd765e60f70ac24a57c4ec4535428a849170c1a
zhaojiangbing <1846027296@qq.com>
2023-06-12 20:00:21 +0800

    fix: test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	preset: '@vue/cli-plugin-unit-jest'
+	// preset: '@vue/cli-plugin-unit-jest'
 	// testMatch: ['**/tests/unit/**/*.spec.js'],
 	// // 添加以下内容以支持ES6模块：
 	// transform: {
